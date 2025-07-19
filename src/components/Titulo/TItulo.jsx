eb68113c3f86922f13c8e8ad8d90f6ed1c903a8e
alme-nutricao <dev@almenutricao.com.br>
2025-07-19 18:24:52 -0300

    ajuste8

--- src/components/Titulo/TItulo.jsx
+++ src/components/Titulo/TItulo.jsx
@@ -2,7 +2,7 @@ import './Titulo.css';
 
 function Titulo(props){
     return (
-        <div className='container-titulo'>
+        <div className='container-titulo rt'>
             <h1>{props.titulo}</h1>
         </div>
     )
